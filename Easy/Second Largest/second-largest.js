a7f76a58151b6d69558b781f84fb2598ebbc5590
Abdul Vahab A A <111953542+abdulvahabaa@users.noreply.github.com>
2024-05-27 00:29:24 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Second Largest/second-largest.js
@@ -0,0 +1,90 @@
+//{ Driver Code Starts
+//Initial Template for javascript
+
+
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+function printList(res,n){
+    let s="";
+    for(let i=0;i<n;i++){
+        s+=res[i];
+        s+=" ";
+    }
+    console.log(s);
+}
+
+
+function main() {
+    let t = parseInt(readLine());
+    let i = 0;
+    for(;i<t;i++)
+    {
+        let n = parseInt(readLine());
+        let arr = new Array(n);
+        let input_ar1 = readLine().split(' ').map(x=>parseInt(x));
+        for(let i=0;i<n;i++){
+            arr[i] = input_ar1[i];
+        }
+        let obj = new Solution();
+        let res = obj.print2largest(arr, n);
+        console.log(res);
+        
+    }
+}// } Driver Code Ends
+
+
+
+
+// } Driver Code Ends
+
+
+//User function Template for javascript
+
+
+/**
+ * @param {number[]} arr
+ * @param {number} n
+ * @returns {number}
+*/
+
+class Solution{
+    print2largest(arr,n){
+        let first=-1
+        let second= -1
+       for(let i=0;i<n;i++){
+           if(arr[i]>first){
+               second=first
+               first=arr[i]
+           }else if( arr[i]> second && arr[i]!== first){
+               second = arr[i]
+           }
+       }
+       
+       return second
+       
+    }
+    
+}
+
